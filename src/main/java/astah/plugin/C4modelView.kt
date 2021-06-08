c7258b9200ce48bc8924151dde4bbf0588ac99e7
toshinoritakai <toshinori.takai@change-vision.com>
2021-06-08 15:13:01 +0900

    ノートや画像の移動に応じて自動的に位置を合わせるようにした

--- src/main/java/astah/plugin/C4modelView.kt
+++ src/main/java/astah/plugin/C4modelView.kt
@@ -1,5 +1,7 @@
 package astah.plugin
 
+import com.change_vision.jude.api.inf.AstahAPI
+import com.change_vision.jude.api.inf.presentation.IPresentation
 import com.change_vision.jude.api.inf.project.ProjectEvent
 import com.change_vision.jude.api.inf.project.ProjectEventListener
 import com.change_vision.jude.api.inf.ui.IPluginExtraTabView
@@ -14,6 +16,7 @@ import javax.swing.JPanel
 class C4modelView : JPanel(), IPluginExtraTabView, ProjectEventListener, ActionListener {
     val buttonRefreshBasedOnModel = ButtonRefresh("Refresh (based on model)")
     val buttonRefreshBasedOnText = ButtonRefresh("Reflect c4model texts to the model")
+    var isAlinging: Boolean = false
 
     init {
         val panelButtons = JPanel()
@@ -23,6 +26,16 @@ class C4modelView : JPanel(), IPluginExtraTabView, ProjectEventListener, ActionL
         buttonRefreshBasedOnModel.addActionListener(this)
         buttonRefreshBasedOnText.addActionListener(this)
         add(panelButtons)
+        addProjectEventListener()
+    }
+
+    private fun addProjectEventListener() {
+        try {
+            val projectAccessor = AstahAPI.getAstahAPI().projectAccessor
+            projectAccessor.addProjectEventListener(this)
+        } catch (e: ClassNotFoundException) {
+            e.printStackTrace()
+        }
     }
 
     override fun actionPerformed(e: ActionEvent) {
@@ -44,5 +57,20 @@ class C4modelView : JPanel(), IPluginExtraTabView, ProjectEventListener, ActionL
     override fun deactivated() {}
     override fun projectOpened(p0: ProjectEvent?) {}
     override fun projectClosed(p0: ProjectEvent?) {}
-    override fun projectChanged(p0: ProjectEvent?) {}
+    override fun projectChanged(p0: ProjectEvent?) {
+        if (!isAlinging) {
+            p0?.let { projectEvent ->
+                val presentationUnits = projectEvent.projectEditUnit.map { it.entity }.filterIsInstance<IPresentation>()
+                if (presentationUnits.any { it.type == "Text" }) {
+                    isAlinging = true
+                    buttonRefreshBasedOnModel.push(AstahAccessor.getCurrentDiagram(), ReferenceSource.TEXT_NODE)
+                    isAlinging = false
+                } else if (presentationUnits.any { it.type == "Class" }) {
+                    isAlinging = true
+                    buttonRefreshBasedOnModel.push(AstahAccessor.getCurrentDiagram(), ReferenceSource.MODEL)
+                    isAlinging = false
+                }
+            }
+        }
+    }
 }
